2e20bd271ec7e8029306f5113195e731780a988b
Aleksandar Tadic <aleksandartadic.ns@gmail.com>
2018-04-09 11:42:20 +0200

    Burger menu button animation added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById("planets-nav").addEventListener("click", function(e) {
-    // console.log(e.target);
     if (e.target.tagName == "H3") {
         if (e.target.nextElementSibling.style.display === "block") {
             e.target.nextElementSibling.style.display = "none";
@@ -12,6 +11,8 @@ document.getElementById("planets-nav").addEventListener("click", function(e) {
 });
 
 
-document.getElementById("nav-button").addEventListener("click", function(e) {
+document.getElementById("nav-button").addEventListener("click", function() {
     document.getElementById("sidebar").classList.toggle("visible");
+
+    document.getElementById("nav-button").classList.toggle("open");
 });
